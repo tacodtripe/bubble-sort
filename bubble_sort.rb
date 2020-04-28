b5909d54a78824cdcc774164b0cf644b5c8f1a39
tacodtripe <55043949+tacodtripe@users.noreply.github.com>
2020-04-28 10:49:08 -0600

    fixed linter errors

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,12 +1,12 @@
 def bubble_sort(input_array)
   n = input_array.length
-  for i in 0..n - 1
-    for a in 0..n - 2 - i
-      if input_array[a] > input_array[a + 1]
-        number_holder = input_array[a]
-        input_array[a] = input_array[a + 1]
-        input_array[a + 1] = number_holder
-      end
+  (0..n - 1).each do |i|
+    (0..n - 2 - i).each do |a|
+      next unless input_array[a] > input_array[a + 1]
+
+      number_holder = input_array[a]
+      input_array[a] = input_array[a + 1]
+      input_array[a + 1] = number_holder
     end
   end
   input_array
@@ -15,24 +15,23 @@ p bubble_sort([4, 3, 78, 2, 0, 2])
 
 def bubble_sort_by(input_array)
   n = input_array.length
-  for i in 0..n - 1
-    for a in 0..n - 2 - i
+  (0..n - 1).each do |i|
+    (0..n - 2 - i).each do |a|
       words = yield(input_array[a], input_array[a + 1]).positive?
-      if words
-        number_holder = input_array[a]
-        input_array[a] = input_array[a + 1]
-        input_array[a + 1] = number_holder
-      end
-      end
+      next unless words
 
+      number_holder = input_array[a]
+      input_array[a] = input_array[a + 1]
+      input_array[a + 1] = number_holder
+    end
   end
   input_array
 end
 
-p bubble_sort_by(["hi", "hello", "hey",]) { |first, second|
+p bubble_sort_by(%w[hi hello hey]) { |first, second|
   first.length - second.length
 }
 
-p bubble_sort_by(%w(my name is Maurice and my Partner is Raul)) { |first, second|
+p bubble_sort_by(%w[my name is Maurice and my Partner is Raul]) { |first, second|
   first.length - second.length
 }
