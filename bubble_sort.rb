49884e54ba6d8315b826c2966277ead782742835
Morrism1 <morrismontana0@gmail.com>
2020-04-28 14:54:13 +0200

    Add bubble_sort_by method using yield keyword

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -12,3 +12,19 @@ def bubble_sort(input_array)
   input_array
 end
 p bubble_sort([4, 3, 78, 2, 0, 2])
+
+def bubble_sort_by(input_array)
+  n = input_array.length
+  for i in 0..n - 1
+    for a in 0..n - 2 - i
+      words = yield(input_array[a], input_array[a + 1]).positive?
+      if words
+        number_holder = input_array[a]
+        input_array[a] = input_array[a + 1]
+        input_array[a + 1] = number_holder
+      end
+      end
+
+  end
+  input_array
+end
